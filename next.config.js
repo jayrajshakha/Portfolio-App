c9a93098773cf9c025b104e6638fd75c7fedab89
Jay Rajshakha <jayrajshakha01@gmail.com>
2023-12-08 19:48:38 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    outputFileTracing : true,
 }
 
 module.exports = nextConfig
--- src/components/ProjectItems.tsx
+++ src/components/ProjectItems.tsx
@@ -1,8 +1,15 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ProjectItem = ({ title, backgroundImg, tech, projectUrl }) => {
+type PropsType = {
+  title: string;
+  backgroundImg: StaticImageData;
+  tech: string;
+  projectUrl: string;
+};
+
+const ProjectItem = ({ title, backgroundImg, tech, projectUrl }: PropsType) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <Image
