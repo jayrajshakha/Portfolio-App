7c59288741bc33ad7975498fed0bde19d213aca0
Jay Rajshakha <jayrajshakha01@gmail.com>
2023-12-15 15:47:52 +0530

    Update

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"WQRKyc9hlwgmQT9otWzpTywCkJaj/g97WDigxOX9aSo=\"}"
--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yngmRPFixMStKWOmZc83vVhl+etwNL3NvpAhrFcW744=\"\n}"
